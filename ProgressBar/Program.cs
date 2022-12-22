2cb3eb23725e1a3e896a82a7e9e13e7f4b86144f
Ferlony <110716986+Ferlony@users.noreply.github.com>
2022-12-22 20:08:10 +0500

    Add files via upload

---
+++ ProgressBar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace Dotnet
+{
+    class Program
+    {
+
+        internal static string ReadFile(FileInfo file, bool textflag=true)
+        {
+            string path = file.FullName;
+            StreamReader reader = new StreamReader(path, Encoding.UTF8);
+            string text = reader.ReadToEnd();
+            if (textflag)
+            {
+                Console.WriteLine($"\nФайл {file.Name}");
+            }
+            //Console.WriteLine(text);
+            reader.Close();
+            return text;
+        }
+        public static void Main(string[] args)
+        {
+            string filePath = @$"..{Path.DirectorySeparatorChar}progbar.txt";
+            FileInfo file = new FileInfo(filePath);
+
+            Console.Write(ReadFile(file, false));
+            
+            while(true)
+            {
+                Console.Clear();
+                Console.Write(ReadFile(file, false));
+                Thread.Sleep(500);
+            }
+
+        }
+
+    }
+}
---
+++ Timer/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+
+namespace Dotnet
+{
+    class Timers
+    {
+        static void Main()
+        {
+            // Create an AutoResetEvent to signal the timeout threshold in the
+            // timer callback has been reached.
+            AutoResetEvent autoEvent = new AutoResetEvent(true);
+            
+            StatusChecker statusChecker = new StatusChecker(10);
+
+            // Create a timer that invokes CheckStatus after one second, 
+            // and every 1/4 second thereafter.
+            Console.WriteLine("time");
+            Timer stateTimer = new Timer(statusChecker.CheckStatus, autoEvent, 1000, 250);
+
+            // // When autoEvent signals, change the period to every half second.
+            // autoEvent.WaitOne();
+            // stateTimer.Change(0, 500);
+            // Console.WriteLine("\nChanging period to .5 seconds.\n");
+
+            // // When autoEvent signals the second time, dispose of the timer.
+            // autoEvent.WaitOne();
+            // stateTimer.Dispose();
+            // Console.WriteLine("\nDestroying timer.");
+        }
+    }
+    class StatusChecker
+{
+    private int invokeCount;
+    private int  maxCount;
+
+    public StatusChecker(int count)
+    {
+        invokeCount  = 0;
+        maxCount = count;
+    }
+
+    // This method is called by the timer delegate.
+    public void CheckStatus(Object stateInfo)
+    {
+        AutoResetEvent autoEvent = (AutoResetEvent)stateInfo;
+        Console.WriteLine("time");
+
+        if(invokeCount == maxCount)
+        {
+            // Reset the counter and signal the waiting thread.
+            invokeCount = 0;
+            autoEvent.Set();
+        }
+    }
+}
+
+}
