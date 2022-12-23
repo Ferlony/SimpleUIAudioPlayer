93c38e99ca29aae6e1bfc094db7ae960d15a9c45
Ferlony <110716986+Ferlony@users.noreply.github.com>
2022-12-23 16:29:41 +0500

    Add files via upload

---
+++ sourceCodeOfUsedProcAndLibs/LProgressBar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace Dotnet
+{
+    class Program
+    {
+
+        internal static string ReadFile(FileInfo file, bool textflag=true)
+        {
+            string path = file.FullName;
+            StreamReader reader = new StreamReader(path, Encoding.UTF8);
+            string text = reader.ReadToEnd();
+            if (textflag)
+            {
+                Console.WriteLine($"\nФайл {file.Name}");
+            }
+            //Console.WriteLine(text);
+            reader.Close();
+            return text;
+        }
+        public static void Main(string[] args)
+        {
+            string filePath = $@".{Path.DirectorySeparatorChar}modules{Path.DirectorySeparatorChar}ProgressBar{Path.DirectorySeparatorChar}progbar.txt";
+            FileInfo file = new FileInfo(filePath);
+
+            
+            while(true)
+            {
+                Console.Clear();
+                Console.Write(ReadFile(file, false));
+                Thread.Sleep(500);
+            }
+
+        }
+
+    }
+}
